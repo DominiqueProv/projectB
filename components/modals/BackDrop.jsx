1991be6da6710ad26f6659e7793a74b38d9d55a0
Dominiqueprov <dominiquepro@hotmail.com>
2023-01-03 20:34:43 -0500

    fix overflow close modal menu

--- components/modals/BackDrop.jsx
+++ components/modals/BackDrop.jsx
@@ -1,7 +1,12 @@
 const BackDrop = ({ setShowModal, showModal }) => {
   return (
     <div
-      onClick={() => setShowModal(false)}
+      onClick={() => {
+        if (typeof window != "undefined" && window.document) {
+          document.body.style.overflow = "unset";
+        }
+        setShowModal(false);
+      }}
       className={`inset-0 absolute bg-black bg-opacity-30 z-10 backdrop-blur-sm ${
         showModal ? "block" : "hidden"
       }`}
