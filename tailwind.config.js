d1242e020bd3a103c8882cab931e3873baa65b33
Dominique Provencher <dprovencher@absolunet.com>
2024-07-08 16:26:23 -0400

    Update tailwind safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  mode: "jit",
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      minHeight: {
-        40: "40px",
-        300: "300px",
-      },
-      maxWidth: {
-        400: "400px",
-      },
-      spacing: {
-        420: "420px",
-      },
-      transitionTimingFunction: {
-        "out-expo": "cubic-bezier(0.16, 1, 0.3, 1)",
-      },
-      fontFamily: {
-        main: ["Unbounded"],
-        manrope: ["Manrope"],
-      },
-      aspectRatio: {
-        card: "16 / 14",
-      },
+    mode: 'jit',
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}'
+    ],
+    safelist: [
+        {
+            pattern: /grid-cols-\d+/
+        }
+    ],
+    theme: {
+        extend: {
+            minHeight: {
+                40: '40px',
+                300: '300px'
+            },
+            maxWidth: {
+                400: '400px'
+            },
+            spacing: {
+                420: '420px'
+            },
+            transitionTimingFunction: {
+                'out-expo': 'cubic-bezier(0.16, 1, 0.3, 1)'
+            },
+            fontFamily: {
+                main: ['Unbounded'],
+                manrope: ['Manrope']
+            },
+            aspectRatio: {
+                card: '16 / 14'
+            }
+        }
     },
-  },
-  plugins: [],
+    plugins: []
 };
