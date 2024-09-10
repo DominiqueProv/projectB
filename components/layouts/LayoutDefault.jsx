f9128e43d61119c3cebe7071b2d7797a0fb7c599
Dominiqueprov <dominiquepro@hotmail.com>
2024-09-10 15:31:19 -0400

    Fix dashboard bg

--- components/layouts/LayoutDefault.jsx
+++ components/layouts/LayoutDefault.jsx
@@ -7,8 +7,8 @@ const LayoutDefault = (props) => {
     <>
       <Navbar isDark={isDark} />
       <main
-        className={`max-w-[1920px] min-h-[calc(100vh-255px)] mx-auto px-2.5 md:px-4 sm:pt-8 ${
-          isDark ? "bg-indigo-900" : ""
+        className={`min-h-[calc(100vh-255px)] mx-auto px-2.5 md:px-4 sm:pt-8 ${
+          isDark ? "bg-indigo-900" : "max-w-[1920px]"
         }`}
       >
         {props.children}
